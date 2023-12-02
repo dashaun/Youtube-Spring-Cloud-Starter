3e66642551aee0596d79233c08d4617e61e3721e
honnuanand <anand@honnu.com>
2023-12-02 00:00:42 -0800

    Spring formatting

--- src/main/java/io/clue2solve/spring/cloud/starter/youtube/model/CaptionInfo.java
+++ src/main/java/io/clue2solve/spring/cloud/starter/youtube/model/CaptionInfo.java
@@ -1,8 +1,8 @@
 package io.clue2solve.spring.cloud.starter.youtube.model;
 
 /**
- * Represents the details of a YouTube video Caption.
- * This is extracted from the YouTube API. See https://developers.google.com/youtube/v3/docs/captions
+ * Represents the details of a YouTube video Caption. This is extracted from the YouTube
+ * API. See https://developers.google.com/youtube/v3/docs/captions
  */
 public record CaptionInfo(String id, String language, String trackKind, boolean isDraft, boolean isAutoSynced) {
 }
--- src/main/java/io/clue2solve/spring/cloud/starter/youtube/model/VideoDetails.java
+++ src/main/java/io/clue2solve/spring/cloud/starter/youtube/model/VideoDetails.java
@@ -4,9 +4,9 @@ import java.math.BigInteger;
 import java.util.List;
 
 /**
- * Represents the details of a YouTube video.
- * This is extracted from the YouTube API. See https://developers.google.com/youtube/v3/docs/videos
+ * Represents the details of a YouTube video. This is extracted from the YouTube API. See
+ * https://developers.google.com/youtube/v3/docs/videos
  */
 public record VideoDetails(String id, String title, String description, BigInteger viewCount, BigInteger likeCount,
-                           String duration, int numberOfCaptions, List<String> captionLanguages) {
+		String duration, int numberOfCaptions, List<String> captionLanguages) {
 }
